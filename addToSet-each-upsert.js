2935e817317d6b200b1a759734b04ec9b65e7180
rzymek <rzymek@gmail.com>
2014-09-23 13:25:50 +0200

    removed comments

--- addToSet-each-upsert.js
+++ addToSet-each-upsert.js
@@ -1,31 +1,11 @@
 if (Meteor.isServer) {
     Meteor.startup(function(){
-
         var AddToSet  = new Mongo.Collection('addToSet');
-
         AddToSet.remove({});
 
         AddToSet.update({},{ $addToSet: { array: { $each: [] } } }, {upsert:true});
+
         var result = AddToSet.find().fetch();
-        console.log(JSON.stringify(result));
-        /*
-        Result:
-            [{"_id":"wKGTDDytyxBnWA2sy","array":[{"$each":[]}]}]
-                                                    ^
-                                                    |
-          $each operator inserted into collection --/
-        */
+        console.log(JSON.stringify(git result));
     })
 }
-
-/*
-Expected result:
-    [{"_id":"wKGTDDytyxBnWA2sy","array":[]}]
-
-Compare with raw mongo:
-    db.meteor.addToSet.update({},{ $addToSet: { array: { $each: [] } } }, {upsert:true})
-    db.meteor.addToSet.find()
-
-One liner:
-echo 'db.meteor.addToSet.update({},{ $addToSet: { array: { $each: [] } } }, {upsert:true});db.meteor.addToSet.find();' | meteor mongo
-*/
